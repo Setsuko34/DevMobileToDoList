649f2151373fc608a9ac9a43e88e23a4c5b62024
Setsuko_Aka <maxf34@orange.fr>
2024-02-02 16:06:58 +0100

    add package for bottom sheet

--- ToDoListApp/components/toDo.jsx
+++ ToDoListApp/components/toDo.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { useTheme, Avatar, Button, Card, Text } from "react-native-paper";
+import {
+  useTheme,
+  Avatar,
+  Button,
+  Card,
+  Text,
+  Portal,
+  Modal,
+} from "react-native-paper";
 import { Platform } from "react-native";
 
 const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical";
 
 const ToDoComponent = () => {
   const theme = useTheme();
+  //for the modal body from the doc react-native-paper
+  const [visible, setVisible] = React.useState(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = { backgroundColor: "white", padding: 20 };
+
   const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
-  const RightContent = (props) => <Avatar.Icon {...props} icon={MORE_ICON} />;
+  const RightContent = (props) => (
+    <Button icon={MORE_ICON} mode="contained" onPress={showModal}>
+      {" "}
+    </Button>
+  );
 
   return (
-    <Card style={{ margin: 15, padding: 15 }}>
-      <Card.Title
-        title="Card Title"
-        subtitle="Card Subtitle"
-        left={LeftContent}
-        right={RightContent}
-      />
-      {/* <Card.Content>
+    <View>
+      <Card style={{ margin: 15, padding: 15 }}>
+        <Card.Title
+          title="Card Title"
+          subtitle="Card Subtitle"
+          left={LeftContent}
+          right={RightContent}
+        />
+        {/* <Card.Content>
         <Text>Card content</Text>
       </Card.Content>
 
@@ -26,7 +45,18 @@ const ToDoComponent = () => {
         <Button>Cancel</Button>
         <Button>Ok</Button>
       </Card.Actions> */}
-    </Card>
+      </Card>
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          contentContainerStyle={containerStyle}
+          style={{ margin: 20 }}
+        >
+          <Text>Example Modal. Click outside this area to dismiss.</Text>
+        </Modal>
+      </Portal>
+    </View>
   );
 };
 
